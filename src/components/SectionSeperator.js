42df2cb64447e8695b453cbb2a0702f744dba3a6
chansenhyggedev <chansen.hyggedev@gmail.com>
2022-05-09 14:39:50 -0700

    Fixed font size

--- src/components/SectionSeperator.js
+++ src/components/SectionSeperator.js
@@ -4,7 +4,7 @@ export default function SectionSeperator({heading, btnText}) {
   return (
     <div className="w-full py-24">
         <div className="max-w-2xl mx-auto">
-            <h2 className="text-center text-6xl heading-gradient">{heading}</h2>
+            <h2 className="text-center text-4xl md:text-5xl lg:text-6xl heading-gradient">{heading}</h2>
             <div className="text-2xl text-gray-400 mt-6 max-w-[200px] mx-auto">{btnText}</div>
         </div>
     </div>
